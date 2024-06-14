b50ccf24c179e31ffdd8bbec572ff0276977cf8c
Hridaya Dev Dhungana <hridayadev.dhungana@gmail.com>
2024-06-14 17:32:14 +0545

    improving

--- index.js
+++ index.js
@@ -4,15 +4,30 @@ import bodyParser from "body-parser"
 const port = 3000;
 const app = express();
 
+app.set('view engine', 'ejs');
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/", (req,res)=>{
-    res.render("index.ejs");
+    res.render("index.ejs",{posts:posts});
 })
+
+const posts = [];
+
 app.get("/create",(req,res)=>{
-    var datetime= currentTime;;
-    res.render("create.ejs",{datetime:datetime})
+    res.render("create.ejs");
 })
+
+app.post('/create',(req,res)=>{
+    const post = {
+        time: generateCurrentTime(),
+        heading: req.body.postHeading,
+        content: req.body.postContent
+    }
+    posts.push(post);
+    res.redirect("/");
+})
+
 app.get("/about",(req,res)=>{
     res.render("about.ejs")
 })
@@ -20,12 +35,15 @@ app.listen(port,()=>{
     console.log(`Server is live at port ${port}`);
 })
 
-var currentdate = new Date();
+function generateCurrentTime(){
+    var currentdate = new Date();
     var year = String(currentdate.getFullYear()).slice(-2); 
     var month = ("0" + (currentdate.getMonth() + 1)).slice(-2); 
     var day = ("0" + currentdate.getDate()).slice(-2); 
-    var currentTime = "Date: " + year + "/" + month + "/" + day + " @ " 
+    return "Date: " + year + "/" + month + "/" + day + " @ " 
     + ("0" + currentdate.getHours()).slice(-2) + ":" 
     + ("0" + currentdate.getMinutes()).slice(-2) + ":" 
     + ("0" + currentdate.getSeconds()).slice(-2);
+}
+
     
