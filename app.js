f66ff3c78e140b51d9fc6f8a0551ab3204d91192
Hridaya Dev Dhungana <hridayadev.dhungana@gmail.com>
2024-06-15 20:13:37 +0545

    final

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ import express from "express"
 import bodyParser from "body-parser"
 import fs from "fs";
 import path from "path";
+import { fileURLToPath } from 'url';
+
 
 const port = 3000;
 const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
