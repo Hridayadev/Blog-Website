db38d1ecc2a4c19d766909a201f01303bbf5a809
Hridaya Dev Dhungana <hridayadev.dhungana@gmail.com>
2024-06-15 17:38:45 +0545

    final

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const port = 3000;
 const app = express();
 
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
 
